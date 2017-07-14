acf2514002013dd866e5f53c48d249c9ad12b99b
Yago Pérez Vázquez <yago.perezv@tecnocom.es>
2017-07-14 15:14:54 +0200

    SearchBar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/searchBar';
+
 const API_KEY = 'AIzaSyAXnA_JoQyyguUD7njI1ArXT1cG59p7_Lg';
 
-const App = () => {
-  return <div>Hi!</div>
+const App = () => {  
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
